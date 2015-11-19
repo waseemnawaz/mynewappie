89e47d08de63a80ec042d78b3dfe29575b1f6bfe
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2015-11-19 10:57:34 +0100

    orders pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,3 +2,10 @@ class StaticPagesController < ApplicationController
   def index
   end
 end
+
+class StaticPagesController < ApplicationController
+	def landing_page
+		@featured_product = Product.first
+	end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
