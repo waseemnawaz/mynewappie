5530e2ad257f76cc94fbb3c5d26be25ed41f4f81
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2015-12-02 23:14:39 +0100

    sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,5 +8,14 @@ class StaticPagesController < ApplicationController
 		@featured_product = Product.first
 		@products = Product.limit(3)
 	end
+	def thank_you
+  		@name = params[:name]
+  		@email = params[:email]
+  		@message = params[:message]
+  		ActionMailer::Base.mail(:from => @email,
+      		:to => 'your-email@example.com',
+      		:subject => "A new contact form message from #{@name}",
+      		:body => @message).deliver_now
+	end
 end
 
