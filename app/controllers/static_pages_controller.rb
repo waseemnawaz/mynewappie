838d5762347efc57128864194f13ef0c24d887cd
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2015-12-03 15:28:52 +0100

    moremails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
-  def index
+	def index
   end
-end
-
-class StaticPagesController < ApplicationController
-	def landing_page
+  def landing_page
 		@featured_product = Product.first
 		@products = Product.limit(3)
 	end
@@ -17,5 +14,11 @@ class StaticPagesController < ApplicationController
       		:subject => "A new contact form message from #{@name}",
       		:body => @message).deliver_now
 	end
+  def thank_you
+      @name = params[:name]
+      @email = params[:email]
+      @message = params[:message]
+      UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 end
 
