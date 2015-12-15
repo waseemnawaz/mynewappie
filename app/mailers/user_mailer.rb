98fff13eb5dc0cc8a71a0536918c95c9b6fae411
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2015-12-15 20:09:39 +0100

    automated welcome mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,9 @@ class UserMailer < ActionMailer::Base
         :to => 'waseemhijodenawaz@yahoo.es',
         :subject => "A new contact form message from #{name}")
   end
+  def welcome(user)
+  @appname = "Bike Shop"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
+  end
 end
