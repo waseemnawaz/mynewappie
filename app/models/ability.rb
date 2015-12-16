ac1fd494f78ddf6400aa5671aacc509f6c73482f
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2015-12-16 14:57:10 +0100

    automated email

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,15 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
 
-    if user.role?(:admin)
-        can :manage, :all
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
     end
+
+    #if user.role?(:admin)
+        #can :manage, :all
+    #end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
