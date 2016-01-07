141d72640831b6b03a0645fc0ca268d15a0131e4
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-01-07 11:24:10 +0100

    unit testing validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,9 +1,27 @@
 require 'rails_helper'
 
 describe Product do
-	context  "when the product has comments" do
+	context "when the product has comments" do
+		before do
+			@product = Product.create!(name: "ugly bike")
+			@user = User.create!(email: "goaway@yahoo.es", password: "anythinggoes")
+			@product.comments.create!(rating: 3, user: @user, body: "Awful bike!")
+		end
+		it "returns the average rating of all comments" do
+			expect(@product.average_rating).to eq 3
+		end
 	end
+	context "when the product has a description" do
+		before do
+			@product = Product.create!(description: "green bike")
+		end
+		it "should not be valid" do
+			expect(@product).not_to be_valid
+		end
+	end
+	
+
 end
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+
+
