70a966cf9f914548a8823b018221771ee8e9af46
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-01-07 12:54:11 +0100

    Unit testing final

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,6 +6,8 @@ describe Product do
 			@product = Product.create!(name: "ugly bike")
 			@user = User.create!(email: "goaway@yahoo.es", password: "anythinggoes")
 			@product.comments.create!(rating: 3, user: @user, body: "Awful bike!")
+			@product.comments.create!(rating: 1, user: @user, body: "nice bike!")
+			@product.comments.create!(rating: 5, user: @user, body: "this bike!")
 		end
 		it "returns the average rating of all comments" do
 			expect(@product.average_rating).to eq 3
@@ -13,7 +15,7 @@ describe Product do
 	end
 	context "when the product has a description" do
 		before do
-			@product = Product.create!(description: "green bike")
+			@product = Product.create(name: nil, description: "green bike")
 		end
 		it "should not be valid" do
 			expect(@product).not_to be_valid
