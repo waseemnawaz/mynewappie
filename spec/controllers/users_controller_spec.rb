c3a696d6cb3db20a00a1b1dbb672c707d4cbc223
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-01-08 12:09:41 +0100

    more tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,14 +9,15 @@ describe UsersController, :type => :controller do
   describe "GET #show" do
      context "User is logged in" do
      	before do
-     	sign_in @user
+     		sign_in @user
+     	end
      	it "loads correct user details" do
-     		get :show, @user.id
+     		get :show, id: @user.id
      		expect(response).to have_http_status(200)
      		expect(assigns(:user)).to eq @user
      	end
      end
-  end
+  
 
      context "No user is logged in" do
        it "redirects to login" do
