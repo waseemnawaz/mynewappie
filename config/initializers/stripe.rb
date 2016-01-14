ccc69d4a19a3d8891bda85259afd87005d97d525
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-01-14 20:50:51 +0100

    more stripe changes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,13 @@
-#if Rails.env.production?
-  #Rails.configuration.stripe = {
-    #:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-    #:secret_key => ENV['STRIPE_SECRET_KEY']
-  #}
-#else
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['pk_test_KuPaPAVDIBGW5K3M59I4OhXU'],
+    :secret_key => ENV['sk_test_202pEJHR6cjZnb1kqY4Dt4cO']
+  }
+else
   Rails.configuration.stripe = {
     :publishable_key => 'pk_test_KuPaPAVDIBGW5K3M59I4OhXU',
     :secret_key => 'sk_test_202pEJHR6cjZnb1kqY4Dt4cO'
   }
-#end
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
