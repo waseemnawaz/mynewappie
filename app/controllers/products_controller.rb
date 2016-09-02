1a7ccd98f75b38936fac91f9e3c8d557da7c07ed
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-09-02 15:38:59 +0200

    LIKE

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
   def index
     if params[:q]
       search_term = params[:q]
-      @products = Product.where("name ilike ?", "%#{search_term}%")
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
     else
       @products = Product.all
     end
